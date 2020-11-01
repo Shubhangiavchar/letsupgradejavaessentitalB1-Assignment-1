18bfbc17b5bd2f495890b5379c876c009aac8e7f
Shubhangiavchar <66524935+Shubhangiavchar@users.noreply.github.com>
2020-11-01 15:56:29 +0530

    Day2 Assignment(java essentials)

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.firstJava;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Employee e=new Employee();
+        e.name="shubhangi";
+        e.age=20;
+        e.city="Chikhli";
+        e.Display();
+        Employee e1=new Employee();
+        e1.name="Ashwini";
+        e1.age=21;
+        e1.city="buldhana";
+        e1.Display();
+    }
+}
